077040fa4a5f16e0a4642ad15b89a8d937e0c991
Chloe Law <chloelaw75@hotmail.com>
2023-04-13 13:54:35 -0400

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,2 +1,89 @@
-package PACKAGE_NAME;public class User {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.BufferedWriter;
+
+class User {
+
+    private static final String FILENAME = "users.txt";
+
+    public static String getUsername(String username) {
+        try {
+            Scanner scanner = new Scanner(new File(FILENAME));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] tokens = line.split(",");
+                if (tokens[0].equals(username)) {
+                    scanner.close();
+                    return tokens[0];
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static boolean setUsername(String username, String password) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FILENAME, true));
+            writer.write(username + "," + password);
+            writer.newLine();
+            writer.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static String getPassword(String username) {
+        try {
+            Scanner scanner = new Scanner(new File(FILENAME));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] tokens = line.split(",");
+                if (tokens[0].equals(username)) {
+                    scanner.close();
+                    return tokens[1];
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static boolean setPassword(String username, String password) {
+        try {
+            Scanner scanner = new Scanner(new File(FILENAME));
+            StringBuilder builder = new StringBuilder();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] tokens = line.split(",");
+                if (tokens[0].equals(username)) {
+                    builder.append(username).append(",").append(password).append("\\n");
+                } else {
+                    builder.append(line).append("\\n");
+                }
+            }
+            scanner.close();
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FILENAME));
+            writer.write(builder.toString());
+            writer.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void Message(String message) {
+        System.out.println(message);
+    }
 }
